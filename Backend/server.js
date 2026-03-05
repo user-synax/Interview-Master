ec212f7165fdbae9afca037d50bc0558df425625
User Synax <usersynax@gmail.com>
2026-03-05 18:09:43 +0530

    PORT Fix

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,10 @@
 require("dotenv").config()
 const app = require("./src/app")
 const connectToDB = require("./src/config/database")
-
+const PORT = process.env.PORT
 connectToDB()
 
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000")
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
 })
